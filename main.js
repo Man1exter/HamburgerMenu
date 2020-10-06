ed2383ba8e25660cb204a33974aac31401bcb633
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-06 12:26:39 +0200

    add one functino and boxer

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const move = document.querySelector(".rect");
+const changer = document.querySelector(".close");
+const bars = document.querySelector(".fa-bars")
+const times = document.querySelector(".fa-times")
+
+
+
+const shows = () => {
+
+}
+
+
+changer.addEventListener("click",shows);
